086aece69508918ebc606640049f88afb05b904c
Morozov Vladyslav <vladmorozov32@outlook.com>
2024-01-28 22:09:21 +0200

    fix path problem

--- src/app/top-services-content.service.ts
+++ src/app/top-services-content.service.ts
@@ -10,32 +10,32 @@ export class TopServicesContentService {
     {
       title: 'Site Layouts',
       subtitle: 'DESIGN',
-      image: '../../assets/images/top-services-images/first-image.png',
+      image: 'assets/images/top-services-images/first-image.png',
     },
     {
       title: 'Interior Design',
       subtitle: 'DESIGN',
-      image: '../../assets/images/top-services-images/second-image.png',
+      image: 'assets/images/top-services-images/second-image.png',
     },
     {
       title: '3D Rendering',
       subtitle: 'DESIGN',
-      image: '../../assets/images/top-services-images/third-image.png',
+      image: 'assets/images/top-services-images/third-image.png',
     },
     {
       title: 'Permit Expediting',
       subtitle: 'PERMITTING',
-      image: '../../assets/images/top-services-images/forth-image.png',
+      image: 'assets/images/top-services-images/forth-image.png',
     },
     {
       title: 'Construction Docs',
       subtitle: 'DOCUMENTATION',
-      image: '../../assets/images/top-services-images/fifth-image.png',
+      image: 'assets/images/top-services-images/fifth-image.png',
     },
     {
       title: 'Site Investigations',
       subtitle: 'DUE DILIGENCE',
-      image: '../../assets/images/top-services-images/sixth-image.png',
+      image: 'assets/images/top-services-images/sixth-image.png',
     },
     // Add more features as needed
   ];
